163e7e27db9888c50b336a2cd5e97f4907335702
User9644 <141528905+User9644@users.noreply.github.com>
2024-08-11 21:58:42 +0200

    Aktualisieren von m.js

--- scripts/m.js
+++ scripts/m.js
@@ -1,8 +1,8 @@
 function loadModule(modules) {
     for(let i = 0; i < modules.length; i ++) {
         const s = document.createElement('script');
-script.src = 'https://user9644.github.io/scripts/' + modules[i] + '.js';
-script.type = 'text/javascript';
+s.src = 'https://user9644.github.io/scripts/' + modules[i] + '.js';
+s.type = 'text/javascript';
 document.head.appendChild(s);
     }
 }
