cd6337d538ce8bef587d1a9406e5d3194563e6b8
User9644 <141528905+User9644@users.noreply.github.com>
2024-08-12 18:02:46 +0200

    -

---
+++ data/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => loadModule(
+    'mpf'
+));
+
+//document.documentElement.style.setProperty("--backgroundColor", "white");
+//document.documentElement.style.setProperty("--color", "black");
--- scripts/m.js
+++ scripts/m.js
@@ -1,4 +1,4 @@
-function loadModule(modules) {
+function loadModule(...modules) {
     for(let i = 0; i < modules.length; i ++) {
         const s = document.createElement('script');
 s.src = 'https://user9644.github.io/scripts/' + modules[i] + '.js';
